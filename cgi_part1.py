2642d39148fa3e60a1c58390d48b5f627eee3cae
SAURABH KSHIRSAGAR <32033617+saurabhpk3719@users.noreply.github.com>
2019-01-05 19:30:45 +0530

    Add files via upload

---
+++ cgi_part1.py
@@ -0,0 +1,12 @@
+#!C:\Users\shree\AppData\Local\Programs\Python\Python37-32\python.exe
+print ("Content-type:text/html\r\n\r\n")
+
+
+print ('<html>')
+print('<head>')
+print('<title>Hello world</title>')
+print('</head>')
+print('<body>')
+print('<h2>Hello world this is saurabh kshirsagar</h2>')
+print('</body>')
+print('</html>')
